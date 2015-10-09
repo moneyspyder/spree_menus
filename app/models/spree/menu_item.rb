eb42235905c75e54b0b7961e05d54ddbff362dc9
Warren Sangster <warren@moneyspyder.co.uk>
2015-10-09 16:57:36 +0100

    HH-36

--- app/models/spree/menu_item.rb
+++ app/models/spree/menu_item.rb
@@ -1,5 +1,12 @@
 class Spree::MenuItem < ActiveRecord::Base
+  acts_as_list
+
   validates :title, presence: true
+
+  belongs_to :page, class_name: 'Spree::Page'
+
+  has_many :menu_items
+
 
 
 end
